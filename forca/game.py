71d394ffc50b46c9241662d5d0496ad69e318efb
oziel-fc <ozielfc.oficial@gmail.com>
2025-05-31 01:40:32 -0300

    Adicionando a função de limpar a tela ao botão Start e adicionando mais comentários

--- forca/game.py
+++ forca/game.py
@@ -5,11 +5,8 @@ import webbrowser
 
 path = Path(__file__).parent
 
-def abrir_link():
-    webbrowser.open("https://github.com/oziel-fc/Jogo_da_Forca")
 
-
-class JogoForca:
+class HomePage:
     def __init__(self):
         self.window = tk.Tk()
         self.color_bg = "#F2ECCE"
@@ -27,6 +24,7 @@ class JogoForca:
         self.window.configure(bg=self.color_bg)
         self.window.minsize(width=1200, height=900)
 
+    # Configuração tela inicial
     def home_screen(self):
         self.img_path_t = path / "imgs" / "title.png"
         self.img_t = Image.open(self.img_path_t)
@@ -35,8 +33,10 @@ class JogoForca:
         self.title_img = tk.Label(self.window, image=self.img_tk, bg=self.color_bg)
         self.title_img.pack(pady=80)
 
+    # Configuração botão de start funcional
     def start_button(self):
-        self.start_btn = tk.Button(self.window, text="Start", font=("Comic Sans MS", 20), background="#A6E07F", activebackground="#6FAD45", fg="#163600", activeforeground="#163600")
+        self.start_btn = tk.Button(self.window, text="Start", font=("Comic Sans MS", 20), background="#A6E07F", activebackground="#6FAD45", fg="#163600", 
+                                   activeforeground="#163600", command=self.clear_page)
         self.start_btn.place(relx=0.44, rely=0.48, width=200, height=80)
     
     # Créditos
@@ -51,7 +51,11 @@ class JogoForca:
         self.credits_img = tk.Label(self.window, image=self.img_c_tk, bg=self.color_bg)
         self.credits_img.place(relx=0.85, rely=0.8)
         self.credits_img.bind('<Button-1>', self.open_link)
-        
+
+    # Limpar infomações da tela
+    def clear_page(self):
+        for widget in self.window.winfo_children():
+            widget.destroy()
 
     def run(self):
         # Inicia o loop principal
@@ -60,5 +64,5 @@ class JogoForca:
 
 if __name__ == "__main__":
 
-    app = JogoForca()
+    app = HomePage()
     app.run()
